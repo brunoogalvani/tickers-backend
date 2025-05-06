fada6ee06a1241b7677c68fddf067e51b6065a4d
brunoogalvani <bthezolin@gmail.com>
2025-05-05 21:28:30 -0300

    fix CORS

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,7 @@ import { authUser, criarUser, deletarUser, listarUsers } from '../controllers/us
 
 const routes = (app) => {
     app.use(express.json())
-    app.use(cors)
+    app.use(cors())
     app.get('/users', listarUsers)
     app.post('/users', criarUser)
     app.delete('/users/:id', deletarUser)
